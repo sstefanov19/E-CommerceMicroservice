d6efa0bebbe3a4e4c6f0fd9514099e0ab0a26f45
sstefanov <84671344+sstefanov19@users.noreply.github.com>
2025-10-14 19:59:15 +0300

    Initial product service tests

--- products-service/src/main/java/api/products/dto/ProductRequest.java
+++ products-service/src/main/java/api/products/dto/ProductRequest.java
@@ -3,11 +3,13 @@ package api.products.dto;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.math.BigDecimal;
 
+@Builder
 @Getter
 @Setter
 public class ProductRequest {
